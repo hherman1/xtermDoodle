a8e48333da0923dca7b501bf022772dd41bba56c
hunteriam <hhstuy@gmail.com>
2013-11-01 11:26:33 -0400

    new methods

--- world.h
+++ world.h
@@ -1,10 +1,12 @@
-#include "platform.h"
+#include "platforms.h"
 
 #define SCREEN_WIDTH 50
 #define SCREEN_HEIGHT 45
+
 
 typedef enum bool {
 	false,true;
 }
 
 bool isUsed(int x, int y);
+bool update();
