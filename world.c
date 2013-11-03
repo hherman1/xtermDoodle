c1be9bc790effc9a1b3fcc8c654ea29829f84227
tian <tiansrealm@gmail.com>
2013-11-03 16:17:36 -0500

    minor change

--- world.c
+++ world.c
@@ -1,11 +1,15 @@
 #include "world.h"
 
+
+#define SCREEN_WIDTH 50
+#define SCREEN_HEIGHT 45
+#define GAMESPEED_FAST 10
+#define GAMESPEED_SLOW 5
 static char world[SCREEN_HEIGHT][SCREEN_WIDTH];
 static Platform* platforms;
 static int  numPlatforms = 0;
-
-
-
+static int tick = 0;
+static int gameSpeed= GAMESPEED_FAST;
 void newgame(){
 	//init world array
 	int row,col;	
@@ -15,21 +19,27 @@ void newgame(){
 		}
 }
 void updateWorld(){
-	if(needPlatform())
-		generatePlatform();
+	shiftWorldDown();
 	//update display
 	xt_par0(XT_CLEAR_SCREEN);
 	int row,col;
-	for(row = 0; row < SCREEN_HEIGHT; row++)
-		for(col = 0; col < SCREEN_WIDTH; col++){
+	xt_par2(XT_SET_ROW_COL_POS,row=0,col=0);
+	for(row; row < SCREEN_HEIGHT; row++)
+		for(col; col < SCREEN_WIDTH; col++){
 			printf("%c", world[row][col]);
 		}
+	tick++;
 }
 
 static bool needPlatform(){
-	return 1;
+	return (tick > gameSpeed);
 }
 static void generatePlatform(){
 
 }
 static bool isUsed(int x,int y);
+
+static shiftWorldDown(){
+	if(needPlatform())
+		generatePlatform();
+}
