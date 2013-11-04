1f0e9aa0360d6be53160d426678ab6ae77d46511
hunteriam <hhstuy@gmail.com>
2013-11-04 00:01:03 -0500

    bug fixes

--- doodler.h
+++ doodler.h
@@ -1,14 +1,12 @@
 #include "keyboard.h"
 #include "xterm_control.h"
-#include "world.h"
 
 #define JUMP_SPEED 1
-
 typedef struct Doodler {
 	int x;
 	int y;
 	int vy;
-}
-void updateDoodler(int key, Doodler d);
-
+} Doodler;
+Doodler updateDoodler(int key, Doodler *d);
+Doodler newDoodler();
 
